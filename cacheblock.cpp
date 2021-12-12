6810b1c932b31cb8cd1d0c72c81e4b1f86fda6c6
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2021-12-12 16:58:36 -0600

    Porting files to github

---
+++ cacheblock.cpp
@@ -0,0 +1,6 @@
+#include "cacheblock.h"
+
+uint32_t Cache::Block::get_address() const {
+  uint32_t addr = _tag << (_cache_config.get_num_index_bits() + _cache_config.get_num_block_offset_bits());
+  return addr + (_index << _cache_config.get_num_block_offset_bits());
+}
---
+++ utils.cpp
@@ -0,0 +1,19 @@
+#include "utils.h"
+
+uint32_t extract_tag(uint32_t address, const CacheConfig& cache_config) {
+  // TODO
+  
+  return address >> (32 - cache_config.get_num_tag_bits());
+}
+
+uint32_t extract_index(uint32_t address, const CacheConfig& cache_config) {
+  // TODO
+
+  // 2^power - 1 gets power 1s 
+  uint32_t mask = ((1 << cache_config.get_num_index_bits()) - 1) << cache_config.get_num_block_offset_bits();
+  return (mask & address) >> cache_config.get_num_block_offset_bits();
+}
+
+uint32_t extract_block_offset(uint32_t address, const CacheConfig& cache_config) { 
+  return address % (1 << cache_config.get_num_block_offset_bits());
+}
